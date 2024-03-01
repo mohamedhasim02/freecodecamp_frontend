bdb236729e6079c61c4b460778d6a5194eae920d
Mohamed Hasim <147535511+mohamedhasim02@users.noreply.github.com>
2024-03-01 19:54:13 +0530

    Create style.css

---
+++ 2-Build a Markdown Previewer/style.css
@@ -0,0 +1,59 @@
+body {
+  font-family: arial;
+  background-image: url('https://img.freepik.com/free-vector/binary-code-white-background-with-floating-numbers_1017-25331.jpg?w=996&t=st=1669056591~exp=1669057191~hmac=98b11ed25cefc5ad4818799f1dc0a2faa1bf3d920188df67d674476ce0dff998');
+  
+}
+
+#display1{
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  grid-gap: 8px;
+}
+
+#editor1{
+  width: 400px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#preview1{
+  width: 400px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: rgba(0, 0, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#display{
+  display: flex;
+  justify-content: center;
+  grid-gap: 8px;
+}
+
+#editor{
+  width: 396px;
+  height: 500px;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+#preview{
+  width: 396px;
+  height: 500px;
+  border-radius: 10px;
+  border-style: groove;
+  background-color: rgba(255, 255, 255, 0.8);
+  overflow: scroll;
+}
+
+#author{
+  text-align: center;
+  font-size: 12px;
+}
